095604802405a4bda4ad85227b3e313eed94a04b
Kieran Jaggernauth <thejugg63@gmail.com>
2022-07-20 15:46:37 -0500

    Updated migrations & User Model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('uid')->nullable()->unique();
+            $table->text('description')->nullable();
+            $table->integer('opCount')->default(0);
+            $table->float('balance')->default(0);
+            $table->boolean('isAdmin')->default(0);
+            $table->boolean('isTeamLead')->default(0);
+            $table->boolean('isQuartermaster')->default(0);
+            $table->boolean('isActive')->default(1);
+            $table->boolean('isLocked')->default(0);
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
