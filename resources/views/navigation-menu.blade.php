f3ceabfab5195b54bbddbd68c0695b9a89b4cc55
Kieran Jaggernauth <thejugg63@gmail.com>
2022-07-22 18:24:39 -0500

    added highlight to profile image area

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -70,7 +70,7 @@
                 @endif
 
                 <!-- Settings Dropdown -->
-                <div class="ml-3 relative">
+                <div class="ml-3 relative bg-red-200 rounded-full">
                     <x-jet-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
