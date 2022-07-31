446ff58f0a9d3e5c9ab7cd22d4bc952adb017a0d
Kieran Jaggernauth <thejugg63@gmail.com>
2022-07-31 10:59:44 -0500

    Updated dashboard ops card layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,15 +51,23 @@
                                                             <a href="/operations/{{ $operation->id }}">
                                                                 <h2 class="mt-2 mb-2 text-2xl font-bold font-Heading tracking-tight text-gray-900 dark:text-white">{{ $operation->name }}</h2>
                                                             </a>
-                                                            <p class="text-orange-600">{{ $operation->op_date->diffForHumans() }}</p>
-                                                            <p class="text-orange-400">{{ date('g:i a', strtotime($operation->op_date)) }}</p>
-                                                            <p class="text-orange-400 my-1">Type : {{ $operation->type->name }}</p>
+                                                            <div class="container p-4 flex flex-wrap items-center">
+                                                                <div class="mx-auto text-center">
+                                                                    <i class="fa-solid fa-clock text-orange-600 text-2xl"></i>
+                                                                    <p class="text-orange-500 text-sm">{{ $operation->op_date->diffForHumans() }}</p>
+                                                                    <p class="text-orange-500 text-sm">{{ date('g:i a', strtotime($operation->op_date)) }}</p>
+                                                                </div>
+                                                            </div>
+
+                                                            <p class="text-orange-400 text-base">Type : {{ $operation->type->name }}</p>
                                                         </div>
-                                                        <div class="p-4 flex flex-wrap items-center">
-                                                            <a href="/operations/{{ $operation->id }}" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                                                Click to View Details
-                                                                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                                            </a>
+                                                        <div class="container p-4 flex flex-wrap items-center">
+                                                            <div class="mx-auto">
+                                                                <a href="/operations/{{ $operation->id }}" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                                    Click to View Details
+                                                                    <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                                                </a>
+                                                            </div>
                                                         </div>
                                                     </a>
                                                 </div>
