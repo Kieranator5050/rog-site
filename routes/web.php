01dc6c726f85d32df98d0d4f101e7aa7512172b8
Kieran Jaggernauth <thejugg63@gmail.com>
2022-08-13 12:50:03 -0500

    Adjusted middleware grouping

--- routes/web.php
+++ routes/web.php
@@ -35,31 +35,34 @@ Route::middleware([
     //Operation Register\Unregister Routes
     Route::post('/operations/{operation}/user/{user}/unregister',[OperationController::class,'unregister']);
     Route::post('/operations/{operation}/user/{user}',[OperationController::class,'signup']);
+
+    //Admin Middleware group
+    Route::middleware('can:Admin')->group(function (){
+        //Admin Routes
+        Route::get('/admin',[AdminController::class, 'index'])->name('admin');
+
+        //Admin User Routes
+        Route::get('/admin/users',[AdminUserController::class,'index'])->name('admin/users');
+        Route::delete('admin/users/{user}',[AdminUserController::class, 'destroy']);
+        Route::get('/admin/users/{user}/edit',[AdminUserController::class,'edit']);
+        Route::patch('/admin/users/{user}',[AdminUserController::class, 'update']);
+
+
+    });
+
+    //Mission Maker Group
+    Route::middleware('can:MissionMaker')->group(function(){
+        Route::get('/operations',[OperationController::class,'index'])->name('missions');
+        Route::delete('/operations/{operation}',[OperationController::class,'destroy']);
+        Route::patch('/operations/{operation}',[OperationController::class,'update']);
+        Route::get('/operations/{operation}/edit',[OperationController::class, 'edit']);
+        Route::get('/operation/create',[OperationController::class,'create']);
+        Route::post('/operations',[OperationController::class,'store']);
+        Route::post('/operations/{operation}/complete',[OperationController::class,'complete']);
+
+    });
+
 });
 
 
-//Admin Middleware group
-Route::middleware('can:Admin')->group(function (){
-    //Admin Routes
-    Route::get('/admin',[AdminController::class, 'index'])->name('admin');
 
-    //Admin User Routes
-    Route::get('/admin/users',[AdminUserController::class,'index'])->name('admin/users');
-    Route::delete('admin/users/{user}',[AdminUserController::class, 'destroy']);
-    Route::get('/admin/users/{user}/edit',[AdminUserController::class,'edit']);
-    Route::patch('/admin/users/{user}',[AdminUserController::class, 'update']);
-
-
-});
-
-//Mission Maker Group
-Route::middleware('can:MissionMaker')->group(function(){
-    Route::get('/operations',[OperationController::class,'index'])->name('missions');
-    Route::delete('/operations/{operation}',[OperationController::class,'destroy']);
-    Route::patch('/operations/{operation}',[OperationController::class,'update']);
-    Route::get('/operations/{operation}/edit',[OperationController::class, 'edit']);
-    Route::get('/operation/create',[OperationController::class,'create']);
-    Route::post('/operations',[OperationController::class,'store']);
-    Route::post('/operations/{operation}/complete',[OperationController::class,'complete']);
-
-});
